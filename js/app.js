106b9b80f3f5273bb56b460404a7f923db70cd17
efeliz <efeliz@student.fitchburgstate.edu>
2020-08-09 21:39:05 -0400

    contact section

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@
 
 
 // const btnToggle
+const themeSwitch = document.getElementsByClassName("onoffswitch-checkbox")[0];
 
+// console.log(themeSwitch);
+themeSwitch.addEventListener('change',function (event){
+  console.log(event.target.checked);
+})
