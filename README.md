773df730ad8ad71233d09f3ab554e08588167918
Edual Feliz <edwinjfeliz@outlook.com>
2022-11-18 19:37:36 -0500

    updatede coomponets

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# My portfolio
+# [shellbear.me](https://shellbear.me/)
 
-Plesase visit this link [DEMO](https://affectionate-leavitt-fb838e.netlify.app/)
+Source code of my personal website & blog ✨
 
-[![Image from Gyazo](https://i.gyazo.com/86bfc780cecb4efb2e341ecc2bd09b37.png)](https://gyazo.com/86bfc780cecb4efb2e341ecc2bd09b37)
+Check the live version [here](https://shellbear.me/) ⚡️
+
+[![home](./.github/img/home.png)](https://shellbear.me/)
+[![blog](./.github/img/blog.png)](https://shellbear.me/)
+
+## ✨ Features
+
+- Responsive
+- RSS Feed
+- Blog with Notion API
+- Strongly typed
+- Fast thanks to Next.js and Vercel
+- Components made with styled-components
+- GitHub CI to build and lint project
+
+## 📦 Made with
+
+- next.js
+- typescript
+- styled-components
+- styled-system
+- react-notion-x
+- notion API
+
+## 💻 Getting Started
+
+Install dependencies:
+
+```bash
+npm install
+# or
+yarn
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## 🗝 License
+
+This project is licensed under the GPLv3 License - see the [LICENSE](LICENSE) file for details.
---
+++ pages/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+import { Container, Title, Button, Grid, Link, Text } from '@components';
+import styles from '@styles/Home.module.css';
+
+const Home = (): JSX.Element => (
+  <Container>
+    <Container
+      justifyContent="center"
+      alignContent="center"
+      alignItems="center"
+      textAlign="center"
+      paddingY="25px"
+      paddingBottom="40px"
+      gridGap="4rem"
+    >
+      <Container alignItems="center" alignContent="center">
+        <Image
+          src="/me.jpg"
+          alt="Edwin Feliz"
+          width="120px"
+          height="120px"
+          objectFit="cover"
+          className={styles.image}
+        />
+        <Title>Edwin Feliz</Title>
+        <Title
+          fontSize="2rem"
+          color="rgba(0, 0, 0, 0.6)"
+          fontWeight="500"
+          as="h2"
+        >
+          Cloud Engineer
+        </Title>
+      </Container>
+      <Container maxWidth="700px" gridGap="3rem">
+        <Container>
+          <Text textAlign="center">
+            I&apos;m a tech enthusiast with experience in the Powershell and
+            Frontend with reactjs but transitioning my work towards Cloud ☁️
+            &nbsp;
+            {/* <a href="https://shareview.fr">Shareview</a> and living in Bost. */}
+          </Text>
+        </Container>
+        <Link href="https://www.linkedin.com/in/edwin-feliz-4b799620a/">
+          <Button>More about me &rarr;</Button>
+        </Link>
+      </Container>
+    </Container>
+
+    <Container alignItems="center" paddingY="4rem">
+      <Container maxWidth="600px" alignItems="center" alignContent="center">
+        <Title fontSize="3rem" as="h3">
+          Get in touch
+        </Title>
+        <Text textAlign="center">
+          Although I&apos;m always actively looking for projects, my inbox gets
+          full sorry for taking long to reply but feel free to ask me anything!
+        </Text>
+        <Grid
+          gridGap="2rem"
+          marginTop="2rem"
+          gridTemplateColumns={['1fr', 'repeat(2, 1fr)']}
+          justifyItems="stretch"
+          alignItems="stretch"
+        >
+          <Link href="mailto:hello@shellbear.me">
+            <Button width="100%">
+              <motion.span
+                initial={{ display: 'inline-block' }}
+                animate={{ rotate: [0, 14, -8, 14, -4, 10, 0, 0] }}
+                transition={{
+                  repeat: Infinity,
+                  repeatType: 'reverse',
+                  duration: 2.5,
+                }}
+              >
+                👋
+              </motion.span>{' '}
+              Say hello
+            </Button>
+          </Link>
+          <Link
+            target="_blank"
+            href="https://calendly.com/d/g5r-r94-4kq/30-minute-meeting"
+          >
+            <Button
+              width="100%"
+              backgroundColor="rgb(226,232,240)"
+              color="black"
+              variant="secondary"
+            >
+              Schedule a meeting
+            </Button>
+          </Link>
+        </Grid>
+      </Container>
+    </Container>
+  </Container>
+);
+
+export default Home;
